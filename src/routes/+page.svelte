c1b75e1e776f572f8675e4fd720b84701f1904bc
ryan <ryanroberts562@gmail.com>
2024-12-29 18:44:17 +0000

    style: basic form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,21 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+	<div class="space-y-5 p-5">
+		<h1 class="h1 mb-3">Tel Aviv Rehearsal Booker</h1>
+		<input class="input" title="Date" type="date" />
+
+		<div class="flex gap-2 justify-between mb-10">
+			<label for="start-time" class="flex-1">
+				<span class="text-lg">From</span>
+				<input class="input" title="Start Time" type="time" />
+			</label>
+			<label for="end-time" class="flex-1">
+				<span>Until</span>
+				<input class="input" title="End Time" type="time" />
+			</label>
+		</div>
+
+		<div class="flex justify-center">
+			<button type="button" class="btn variant-filled">Submit</button>
+		</div>
 	</div>
 </div>
